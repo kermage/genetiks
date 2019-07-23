c4ed0f0f5a22ab28cb80f8012dc415c6c7e72665
kermage <genealyson.torcende@gmail.com>
2019-07-23 08:46:41 +0800

    export our custom rules

--- config/index.js
+++ config/index.js
@@ -2,5 +2,38 @@
 
 module.exports = {
 	'extends': 'stylelint-config-standard',
-	'plugins': 'stylelint-scss'
+	'plugins': 'stylelint-scss',
+	'rules': {
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				'except': [
+					'blockless-after-same-name-blockless',
+					'first-nested'
+				],
+				'ignore': [
+					'after-comment'
+				],
+				'ignoreAtRules': [
+					'else'
+				]
+			}
+		],
+		'at-rule-no-unknown': null,
+		'block-closing-brace-newline-after': [
+			'always-multi-line',
+			{
+				'ignoreAtRules': [
+					'if',
+					'else'
+				]
+			}
+		],
+		'function-parentheses-space-inside': 'always',
+		'indentation': 'tab',
+		'max-empty-lines': 3,
+		'media-feature-parentheses-space-inside': 'always',
+		'scss/at-rule-no-unknown': true,
+		'value-list-max-empty-lines': 1
+	}
 };
