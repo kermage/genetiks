ef81a48a7b35aa6837c2103925e92d97c3878f3c
kermage <genealyson.torcende@gmail.com>
2024-03-09 12:27:51 +0800

    a much able selector

--- scripts/query.ts
+++ scripts/query.ts
@@ -1,23 +1,43 @@
 import { extend } from './element';
 
 
-export const one = ( selector: string, $context: ParentNode = document ) => {
+export const one = ( selector: string | Element | NodeListOf<Element> | Array<Element>, $context: ParentNode = document ) => {
 	if ( ! selector ) {
 		return null;
 	}
 
-	const element = $context.querySelector( `:scope ${ selector }` );
+	if ( selector instanceof NodeList || Array.isArray( selector ) ) {
+		return extend( selector[0] );
+	}
+
+	let element: Element | null;
+
+	if ( selector instanceof Element ) {
+		element = selector;
+	} else {
+		element = $context.querySelector( `:scope ${ selector }` );
+	}
 
 	return element ? extend( element ) : null;
 };
 
 
-export const many = ( selector: string, $context: ParentNode = document ) => {
+export const many = ( selector: string | Element | NodeListOf<Element> | Array<Element>, $context: ParentNode = document ) => {
 	if ( ! selector ) {
 		return null;
 	}
 
-	const elements = $context.querySelectorAll( `:scope ${ selector }` );
+	if ( selector instanceof Element ) {
+		return [ extend( selector ) ];
+	}
+
+	let elements: NodeListOf<Element> | Element[] | null;
+
+	if ( selector instanceof NodeList || Array.isArray( selector ) ) {
+		elements = selector;
+	} else {
+		elements = $context.querySelectorAll( `:scope ${ selector }` );
+	}
 
 	return elements ? Array.from( elements ).map( element => extend( element ) ) : null;
 };
